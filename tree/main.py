76684a542b7294ebd2b03d4bd873b6581af0a397
Anish <gamedevak7@gmail.com>
2026-07-22 11:31:42 +0530

    feat: Cloning of repo based on url provided by user

--- tree/main.py
+++ tree/main.py
@@ -1,10 +1,17 @@
 from build_workspace_tree import *
 from json_utils import JSONUtils
-
+from clone_repo import clone_repo
 if __name__ == "__main__":
+    url = input("Enter the url of the repo: ")
+    branch = input("Enter the name of the branch: ")
+    if branch == "main":
+        branch = None
+    name = clone_repo(url,branch)
+    if not name:
+        exit(0)
     ignored_files = ['model.py','build_workspace_tree.py','json_utils.py','main.py','os_utils.py']
-    build_full_workspace_tree(".")
-    obj = JSONUtils(".",ignored_files)
+    build_full_workspace_tree(name)
+    obj = JSONUtils(name,ignored_files)
     obj.get_file_and_folders()
     compile_summaries()
     compile_directory_summaries_from_files()
